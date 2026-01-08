12a9399373f932dcbbae69ba75e0d7469e621693
esmaeilsadeghijob <esmaeilsadeghijob@gmil.com>
2026-01-08 11:26:06 -0500

    add main

--- src/main/java/com/learn/Main.java
+++ src/main/java/com/learn/Main.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext();
+                new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        Parrot p = new Parrot();
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println(p.getName());
+
+//        Parrot p = new Parrot();
     }
 }
---
+++ src/main/java/com/learn/ProjectConfig.java
@@ -0,0 +1,15 @@
+package com.learn;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    Parrot parrot() {
+        Parrot p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+}
