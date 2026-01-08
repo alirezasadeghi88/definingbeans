89f2d3676563b0501dccab5b572bfe4f88ecbb73
esmaeilsadeghijob <esmaeilsadeghijob@gmil.com>
2026-01-08 11:22:19 -0500

    add main

---
+++ src/main/java/com/learn/Main.java
@@ -0,0 +1,12 @@
+package com.learn;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext();
+
+        Parrot p = new Parrot();
+    }
+}
